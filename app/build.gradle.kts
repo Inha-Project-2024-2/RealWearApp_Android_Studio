7627e92930e39ddb007b431352c30cf9788001d2
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2024-09-24 21:28:35 +0900

    build.gradle dependency 완성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -7,6 +10,10 @@ android {
     namespace = "com.newapp.realwearapp"
     compileSdk = 34
 
+    val properties = Properties()
+    properties.load(FileInputStream(rootProject.file("local.properties")))
+
+
     defaultConfig {
         applicationId = "com.newapp.realwearapp"
         minSdk = 24
@@ -15,6 +22,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        //buildConfigField("String", "SIGNALING_SERVER_IP_ADDRESS", properties.getProperty("signalingServerIp"))
     }
 
     buildTypes {
@@ -30,8 +38,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -45,4 +59,27 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Web RTC
+    implementation ("io.getstream:stream-webrtc-android:1.1.3")
+    implementation ("io.getstream:stream-webrtc-android-ui:1.1.3")
+
+    // Fragment
+    implementation ("androidx.fragment:fragment-ktx:1.8.1")
+
+    // Lottie
+    implementation ("com.airbnb.android:lottie:6.4.1")
+
+    // Network
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+
+    // Timber
+    implementation ("com.jakewharton.timber:timber:5.0.1")
+
+    // camera
+    implementation ("androidx.camera:camera-camera2:1.3.4")
+    implementation ("androidx.camera:camera-core:1.3.4")
+    implementation ("androidx.camera:camera-lifecycle:1.3.4")
+    implementation ("androidx.camera:camera-view:1.3.4")
 }
